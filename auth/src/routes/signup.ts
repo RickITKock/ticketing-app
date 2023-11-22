8a80bb9266281e42441769b3fedf09f4214a8e66
Rick Kock <rit.kock@gmail.com>
2023-11-22 21:52:20 +0100

    Add basic error handling at /api/users/signup

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,30 @@
 import express, { NextFunction, Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/users/signup", (req, res) => {
-  res.send("Hi there!");
-});
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    const { email, password } = req.body;
+
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+
+    console.log("Creating a user...");
+
+    res.send({});
+  }
+);
 
 export { router as signupRouter };
