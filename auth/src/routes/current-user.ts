9fcd8b7e64d4d004d24198450d02c0d5640e2e23
Rick Kock <rit.kock@gmail.com>
2023-11-24 15:04:53 +0100

    Check if user is authorized through require-auth middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,10 +1,15 @@
 import express, { NextFunction, Request, Response } from "express";
 import { currentUser } from "../middleware/current-user";
+import { requireAuth } from "../middleware/require-auth";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", currentUser, (req, res) => {
-  res.send({ currentUser: req.currentUser || null });
-});
+router.get(
+  "/api/users/currentuser",
+  currentUser,
+  (req: Request, res: Response) => {
+    res.send({ currentUser: req.currentUser || null });
+  }
+);
 
 export { router as currentUserRouter };
