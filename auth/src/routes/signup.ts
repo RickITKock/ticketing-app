e9856399af2637f65731bd0b2d1f1c1e2f27a516
Rick Kock <rit.kock@gmail.com>
2023-11-25 12:34:30 +0100

    Fix error handling signing up with invalid password and making test succeed

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -11,10 +11,10 @@ router.post(
   "/api/users/signup",
   [
     body("email").trim().isEmail().withMessage("Email must be valid"),
-    // body("password")
-    //   .trim()
-    //   .isLength({ min: 4, max: 20 })
-    //   .withMessage("Password must be between 4 and 20 characters"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
