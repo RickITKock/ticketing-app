9a8ef23925c36340916ddb10e332bbf981508007
Rick Kock <rit.kock@gmail.com>
2023-11-25 14:56:25 +0100

    Test dissalowing duplicate eamils when signing up

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -30,3 +30,21 @@ it("returns a 400 with an invalid password", async () => {
     })
     .expect(400);
 });
+
+it("disallows duplicate emails", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(400);
+});
