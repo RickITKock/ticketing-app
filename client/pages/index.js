653fc70c2acd5cbc606b97b36ca47cec9740c3d4
Rick Kock <rit.kock@gmail.com>
2023-11-27 09:53:08 +0100

    Client: get current user through ingress-controller

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,10 @@
+import axios from "axios";
+
 // Install sass
 // Create pages: sign-in, sign-up, current-user (account)
 
 const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return currentUser ? (
     <h1>You are signed in</h1>
   ) : (
@@ -9,11 +12,51 @@ const LandingPage = ({ currentUser }) => {
   );
 };
 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async (context) => {
   console.log("LANDING PAGE");
+  console.log(context);
 
-  // Create a request to get user so we can check if user is signed in or not
-  return {};
+  if (typeof window === "undefined") {
+    // We are on the serve
+
+    const response = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: {
+          Host: "ticketing.dev",
+        },
+      }
+    );
+
+    return response.data;
+
+    // try {
+    // const { data } = await axios.get({
+
+    //   // baseURL: "ticketing.dev",
+    //   // "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+    //   // headers: {
+    //   //   Host: "ticketing.dev",
+    //   // },
+    // });
+    // const { data } = await axios.get(
+    //   "ingress-nginx-controller.ingress-nginx.svc.cluster.local"
+    // );
+
+    // console.log(data);
+    // return data;
+    // } catch (error) {
+    //   console.log("Error occurred");
+    //   console.error(error);
+    // }
+  } else {
+    // We must be on the browser
+    console.log("On the browser");
+    const { data } = await axios.get("/api/users/currentuser");
+    console.log(data);
+
+    return data;
+  }
 };
 
 export default LandingPage;
