8ed79602e72cc5a375e2585d6f23637585e4fa84
Rick Kock <rit.kock@gmail.com>
2023-11-27 06:05:12 +0100

    Add parameter to landing page component to get current user

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,18 @@
 // Install sass
 // Create pages: sign-in, sign-up, current-user (account)
 
-const LandingPage = () => {
-  return <h1>Landing Page</h1>;
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
 };
 
 LandingPage.getInitialProps = async () => {
-  console.log("EXECUTED");
+  console.log("LANDING PAGE");
+
+  // Create a request to get user so we can check if user is signed in or not
   return {};
 };
 
