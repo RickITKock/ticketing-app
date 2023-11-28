ac107ccf7162576a18938f9f5e54be03d39c44d3
Rick Kock <rit.kock@gmail.com>
2023-11-28 07:34:54 +0100

    Returns an error if an invalid title is provided

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -6,6 +6,14 @@ const router = express.Router();
 router.post(
   "/api/tickets",
   requireAuth,
+  [
+    body("title").not().isEmpty().withMessage("Title is required"),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price must be greater than 0"),
+  ],
+  validateRequest,
+  requireAuth,
   validateRequest,
   (req: Request, res: Response) => {
     console.log(req.currentUser);
