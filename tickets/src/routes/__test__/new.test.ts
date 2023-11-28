dea0cf1caba610528dd708d8b4adba2f0e2334e5
Rick Kock <rit.kock@gmail.com>
2023-11-28 07:36:20 +0100

    Returns an error if an invalid price is provided

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -38,6 +38,23 @@ it("returns an error if an invalid title is provided", async () => {
     .expect(400);
 });
 
-it("returns an error if an invalid price is provided", async () => {});
+it("returns an error if an invalid price is provided", async () => {
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "asldkjf",
+      price: -10,
+    })
+    .expect(400);
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "laskdfj",
+    })
+    .expect(400);
+});
 
 it("creates a ticket with valid inputs", async () => {});
